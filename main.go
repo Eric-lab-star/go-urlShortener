256d0a24766cfb9a53f50e53167e6ac8da1eb403
eric-lab-star <cyon256@icloud.com>
2023-01-05 21:31:38 +0900

    mapHandler

--- main.go
+++ main.go
@@ -2,24 +2,37 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
-	"sync"
 )
 
-type countHandler struct {
-	mu sync.Mutex // guards n
-	n  int
-}
-
-func (h *countHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	h.mu.Lock()
-	defer h.mu.Unlock()
-	h.n++
-	fmt.Fprintf(w, "count is %d\n", h.n)
-}
-
 func main() {
-	http.Handle("/count", new(countHandler))
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	mux := defaultMux()
+	pathsToUrls := map[string]string{
+		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+	}
+	mapHandler := MapHandler(pathsToUrls, mux)
+
+	http.ListenAndServe("localhost:8080", mapHandler)
+}
+func defaultMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", helloHandler)
+	return mux
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "hello world")
+}
+
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+
+		if url, ok := pathsToUrls[path]; ok {
+			http.Redirect(w, r, url, http.StatusFound)
+			return
+		}
+		fallback.ServeHTTP(w, r)
+	}
 }
