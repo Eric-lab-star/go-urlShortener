27399b527622b4ce8c6f3fd1d4a67b24b1ae40cd
eric-lab-star <cyon256@icloud.com>
2023-01-06 14:36:43 +0900

    yaml parser

--- main.go
+++ main.go
@@ -1,40 +1,63 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+
+	"gopkg.in/yaml.v2"
 )
 
 func main() {
-	defaultMux := defaultMux()
-	urlsToPath := map[string]string{
-		"/google":  "https://google.com",
-		"/youtube": "https://youtube.com",
+	yaml := `
+	- path: /urlshort
+	  url: https://github.com/gophercises/urlshort
+	- path: /urlshort-final
+	  url: https://github.com/gophercises/urlshort/tree/solution
+	`
+	yamlHandler, err := YAMLHandler([]byte(yaml), http.NewServeMux())
+	if err != nil {
+		panic(err)
 	}
+	http.ListenAndServe("localhost:8080", yamlHandler)
 
-	http.ListenAndServe("localhost:8080", MapUrlstoPath(urlsToPath, defaultMux))
 }
 
-func home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "hello world")
-}
-
-func defaultMux() *http.ServeMux {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-
-	return mux
-}
-
-func MapUrlstoPath(urlsToPath map[string]string, fallback *http.ServeMux) http.HandlerFunc {
+func MapHandler(pathtoUrl map[string]string, fallback http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-
 		path := r.URL.Path
-		if url, ok := urlsToPath[path]; ok {
+		if url, ok := pathtoUrl[path]; ok {
 			http.Redirect(w, r, url, http.StatusFound)
-			return
 		}
 		fallback.ServeHTTP(w, r)
 	}
+}
 
+func YAMLHandler(yamlBytes []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	pathUrls, err := parseYaml(yamlBytes)
+	if err != nil {
+		return nil, err
+	}
+	pathsToUrls := buildMap(pathUrls)
+	return MapHandler(pathsToUrls, fallback), nil
+}
+
+func buildMap(pathUrls []pathUrl) map[string]string {
+	pathsToUrls := make(map[string]string)
+	for _, pu := range pathUrls {
+		pathsToUrls[pu.Path] = pu.URL
+	}
+	return pathsToUrls
+}
+
+func parseYaml(data []byte) ([]pathUrl, error) {
+	var pathUrls []pathUrl
+	err := yaml.Unmarshal(data, &pathUrls)
+	if err != nil {
+		return nil, err
+	}
+	return pathUrls, nil
+}
+
+type pathUrl struct {
+	Path string `yaml:"path"`
+	URL  string `yaml:"url"`
 }
